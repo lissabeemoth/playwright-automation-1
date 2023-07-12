c49de3aadcbd4f0f2f957a61a18935bd3cbf3eda
lissa <lissa.beemoth@gmail.com>
2023-07-13 00:36:19 +0300

    initial project from test automation with plywright

--- tests/test_dashboard.py
+++ tests/test_dashboard.py
@@ -6,5 +6,6 @@ def test_dashboard_data(desktop_app_auth):
     desktop_app_auth.intercept_request('**/getstat/', payload)
     #desktop_app_auth.intercept_request('http://127.0.0.1:8000/getstat/', payload)
     desktop_app_auth.refresh_dashboard()
-    assert desktop_app_auth.get_total() == '0'
     desktop_app_auth.stop_intercept_request('**/getstat/')
+    assert desktop_app_auth.get_total() == '0'
+
