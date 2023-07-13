17c59eb5472886af9c07f066f81484d95174f899
lissa <lissa.beemoth@gmail.com>
2023-07-13 16:39:15 +0300

    initial project from test automation with plywright

--- page_objects/test_cases.py
+++ page_objects/test_cases.py
@@ -1,13 +1,16 @@
 from playwright.sync_api import Page
+import allure
 
 
 class TestCases:
     def __init__(self, page: Page):
         self.page = page
 
+    @allure.step
     def check_test_exist(self, test_name: str):
         return self.page.query_selector(f'css=tr >> text=\"{test_name}\"') is not None
 
+    @allure.step
     def delete_test_by_name(self, test_name: str):
         row = self.page.query_selector(f'*css=tr >> text=\"{test_name}\"')
         row.query_selector('.deleteBtn').click()
