ce91cf750e7386f60c75fdd1fbed3c2effc107bb
lissa <lissa.beemoth@gmail.com>
2023-07-13 16:39:42 +0300

    initial project from test automation with plywright

--- tests/test_dashboard.py
+++ tests/test_dashboard.py
@@ -2,7 +2,7 @@ import json
 import allure
 
 
-@allure.step
+@allure.title('Test dashboard data')
 def test_dashboard_data(desktop_app_auth):
     payload = json.dumps({"total": 0, "passed": 0, "failed": 0, "norun": 0})
     desktop_app_auth.intercept_request('**/getstat/', payload)
